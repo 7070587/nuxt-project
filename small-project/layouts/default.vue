74201edf76fb6df49503bf69f0259cbf8c49fb37
Tina.Lin <7070587@gmail.com>
2020-11-30 15:28:15 +0800

    layouts default | update emit and prop value to Navigation components

--- small-project/layouts/default.vue
+++ small-project/layouts/default.vue
@@ -1,8 +1,28 @@
 <template>
     <div>
+        <TheHeader @sidenavToggle='displaySideNav = !displaySideNav' />
+        <TheSidenav
+            :show='displaySideNav'
+            @close='displaySideNav = false'
+        />
+
         <Nuxt />
     </div>
 </template>
+
+<script>
+import TheHeader from "@/components/Navigation/TheHeader.vue";
+import TheSidenav from "@/components/Navigation/TheSidenav.vue";
+
+export default {
+    comments: { TheHeader, TheSidenav },
+    data() {
+        return {
+            displaySideNav: false,
+        };
+    },
+};
+</script>
 
 <style>
 html {
