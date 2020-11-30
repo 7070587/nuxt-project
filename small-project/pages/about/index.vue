2dc9f54ab6706730a24df1308e3f6d1450f312cc
Tina.Lin <7070587@gmail.com>
2020-11-30 11:45:40 +0800

    create pages | add about / admin / posts pages

---
+++ small-project/pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="about-page">
+        <h1>This is my absolutely fantastic "About Me" Page!</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis odit inventore voluptatum quis nam totam nisi! Inventore iste adipisci incidunt!</p>
+        <p>Molestias, dolore et unde, officia repellendus in cupiditate porro saepe itaque tempore facere, deleniti ipsum aperiam quas maiores fugiat veritatis corrupti dolorum quis eaque nulla blanditiis!</p>
+        <p>Sit iusto suscipit a quasi cum porro doloremque incidunt ea nostrum, officia accusamus tempora placeat nihil.</p>
+        <p>Distinctio pariatur asperiores sunt necessitatibus, neque iusto harum ullam accusamus soluta voluptas mollitia nesciunt vel eum assumenda tempore ratione velit! Aliquam iusto nostrum voluptas maxime repellat eligendi iure. Libero explicabo odit aliquam!</p>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.about-page {
+    text-align: center;
+    padding: 2rem;
+}
+</style>
