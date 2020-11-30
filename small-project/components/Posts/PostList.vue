a16e1e5489ba5360d58472ff22ba3c55a71bde46
Tina.Lin <7070587@gmail.com>
2020-11-30 18:08:17 +0800

    pages / admin / index + component / post / PostList | update prop value is Admin

--- small-project/components/Posts/PostList.vue
+++ small-project/components/Posts/PostList.vue
@@ -1,24 +1,48 @@
 <template>
-        <section class="post-list">
-            <PostPreview
-                id="4"
-                title='Good!'
-                previewText='Good Job!'
-                thumbnail='https://images.unsplash.com/photo-1506542551059-c31e3fb9e32d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80'
-            />
-            <PostPreview
-                id="5"
-                title='Find You'
-                previewText='It us you'
-                thumbnail='https://images.unsplash.com/photo-1551005916-83ed42ca6b49?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
-            />
-            <PostPreview
-                id="6"
-                title='Just Do It'
-                previewText='Do what you want'
-                thumbnail='https://images.unsplash.com/photo-1591713587894-69f26799dd90?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=925&q=80'
-            />
-        </section>
+    <section class="post-list">
+        <PostPreview
+            :isAdmin='isAdmin'
+            id="1"
+            title='How are you'
+            previewText='Wish you'
+            thumbnail='https://images.unsplash.com/photo-1575989356732-a6f467b97e97?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=716&q=80'
+        />
+        <PostPreview
+            :isAdmin='isAdmin'
+            id="2"
+            title='3QQQQ'
+            previewText='Thanks a lot'
+            thumbnail='https://images.unsplash.com/photo-1604513644626-182145958737?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
+        />
+        <PostPreview
+            :isAdmin='isAdmin'
+            id="3"
+            title='Enjoy!'
+            previewText='Have good time'
+            thumbnail='https://images.unsplash.com/photo-1507388412350-cb43abdebced?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80'
+        />
+        <PostPreview
+            :isAdmin='isAdmin'
+            id="4"
+            title='Good!'
+            previewText='Good Job!'
+            thumbnail='https://images.unsplash.com/photo-1506542551059-c31e3fb9e32d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80'
+        />
+        <PostPreview
+            :isAdmin='isAdmin'
+            id="5"
+            title='Find You'
+            previewText='It us you'
+            thumbnail='https://images.unsplash.com/photo-1551005916-83ed42ca6b49?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+        />
+        <PostPreview
+            :isAdmin='isAdmin'
+            id="6"
+            title='Just Do It'
+            previewText='Do what you want'
+            thumbnail='https://images.unsplash.com/photo-1591713587894-69f26799dd90?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=925&q=80'
+        />
+    </section>
 </template>
 
 <script>
@@ -26,6 +50,12 @@ import PostPreview from "@/components/Posts/PostPreview.vue";
 
 export default {
     comments: { PostPreview },
+    props: {
+        isAdmin: {
+            type: Boolean,
+            required: false,
+        },
+    },
 };
 </script>
 
