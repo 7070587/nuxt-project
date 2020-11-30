4f9672233c4c8d933586f27fbffafebd4984b1c4
Tina.Lin <7070587@gmail.com>
2020-11-30 18:07:13 +0800

    pages / index | update use PostList component

--- small-project/pages/index.vue
+++ small-project/pages/index.vue
@@ -4,35 +4,19 @@
             <h1>Get the latest nature images!</h1>
         </section>
         <section class="featured-posts">
-            <PostPreview
-                id="1"
-                title='How are you'
-                previewText='Wish you'
-                thumbnail='https://images.unsplash.com/photo-1575989356732-a6f467b97e97?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=716&q=80'
-            />
-            <PostPreview
-                id="2"
-                title='3QQQQ'
-                previewText='Thanks a lot'
-                thumbnail='https://images.unsplash.com/photo-1604513644626-182145958737?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-            />
-            <PostPreview
-                id="3"
-                title='Enjoy!'
-                previewText='Have good time'
-                thumbnail='https://images.unsplash.com/photo-1507388412350-cb43abdebced?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80'
-            />
+            <PostList />
         </section>
     </div>
 </template>
 
 <script>
-import PostPreview from "@/components/Posts/PostPreview.vue";
+import PostList from "@/components/Posts/PostList.vue";
 
 export default {
-    comments: { PostPreview },
+    comments: { PostList },
 };
 </script>
+
 
 <style scoped>
 .intro {
