058cbff4673fe5e6758c6ef1f604170f61da0d08
Tina.Lin <7070587@gmail.com>
2020-11-30 18:20:50 +0800

    pages / admin | add admin layout

--- small-project/pages/admin/index.vue
+++ small-project/pages/admin/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="admin-page">
         <section class="new-post">
-            <button @click="$router.push('/admin/new-post')">Create Post</button>
+            <AppButton @click="$router.push('/admin/new-post')">Create Post</AppButton>
         </section>
 
         <section class="existing-posts">
@@ -15,9 +15,11 @@
 
 <script>
 import PostList from "@/components/Posts/PostList.vue";
+import AppButton from "@/components/UI/AppButton.vue";
 
 export default {
-    comments: { PostList },
+    comments: { PostList, AppButton },
+    layout: "admin",
 };
 </script>
 
