2bdc2888e3509240ba3f521335917b8bd2d88daa
Tina.Lin <7070587@gmail.com>
2020-12-03 10:30:52 +0800

    pages / admin  / _postId / index  | udpate use axios to  get single post data and update post data, after success, go to /admin page

--- small-project/pages/admin/_postId/index.vue
+++ small-project/pages/admin/_postId/index.vue
@@ -1,27 +1,44 @@
 <template>
     <div class="admin-post-page">
         <section class="update-form">
-            <AdminPostForm :post='loadedPost' />
+            <AdminPostForm
+                :post='postData'
+                @submit='submit'
+            />
         </section>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+
 import AdminPostForm from "@/components/Admin/AdminPostForm.vue";
 
 export default {
     components: { AdminPostForm },
-    layout: "admin",
-    data() {
-        return {
-            loadedPost: {
-                author: "sci",
-                title: "awesome post",
-                thumbnailLink:
-                    "https://images.unsplash.com/flagged/photo-1591472672704-548ed9ec7611?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1353&q=80",
-                content: "Super Amazing!!",
-            },
-        };
+    // layout: "admin",
+
+    async asyncData(context) {
+        const url = `https://nuxt-blog-58689.firebaseio.com/posts/${context.params.postId}.json`;
+        let { data } = await axios.get(url).catch((e) => context.error(e));
+
+        return { postData: data };
+    },
+
+    methods: {
+        async submit(editedPost) {
+            const url = `https://nuxt-blog-58689.firebaseio.com/posts/${this.$route.params.postId}.json`;
+            const param = { ...editedPost, updatedDate: new Date() };
+
+            let res = await axios
+                .put(url, param)
+                .catch((e) => console.error(e));
+
+            if (res?.statusCode === 200) {
+                this.$router.push("/admin");
+                // console.log("save success => ", res?.data);
+            }
+        },
     },
 };
 </script>
