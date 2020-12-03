b0467aa4e046e17d92aaa7c3afb0437f75498743
Tina.Lin <7070587@gmail.com>
2020-12-03 13:57:42 +0800

    nuxt.config | update head content

--- small-project/nuxt.config.js
+++ small-project/nuxt.config.js
@@ -1,11 +1,15 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: "small-project",
+    title: "My Blog",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      {
+        hid: "description",
+        name: "description",
+        content: "My Nuxt Web Development Blog"
+      }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
